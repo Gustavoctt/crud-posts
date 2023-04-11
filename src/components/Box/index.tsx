eb6f31805533743becde5a7ee35859a8669b151d
Gustavoctt <gustavocarrertartare@gmail.com>
2023-04-10 21:24:36 -0300

    feat: create Box component

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  background-color: #ffffff;
+  padding: 24px;
+  border-radius: 16px;
+  border: 1px solid #cccccc;
+`;
+
+export default Box;
